d17de9cac440d30b468c5922cb5d04c877ac81d0
anuja632 <abianu632.com>
2025-12-09 23:42:19 +0530

    ass

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,6 +9,27 @@
   }
 });
 
+const targetDate = new Date("Nov 20, 2026 00:00:00").getTime();
+
+const timer = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000*60*60*24));
+    const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+    const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+    const seconds = Math.floor((distance % (1000*60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours.toString().padStart(2,'0');
+    document.getElementById("minutes").innerText = minutes.toString().padStart(2,'0');
+    document.getElementById("seconds").innerText = seconds.toString().padStart(2,'0');
+
+    if(distance < 0) {
+        clearInterval(timer);
+        document.querySelector(".timer").innerHTML = "<h3>Event Started!</h3>";
+    }
+},1000);
 /* ------------------------
    1. Navbar Scroll Sticky
 --------------------------- */
